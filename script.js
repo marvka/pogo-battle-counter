bb1c9193d200383899865164c05eb1e33298c048
marvka <marvka@protonmail.com>
2021-02-16 14:45:35 +0000

    Update JS file to Airbnb ESLint standard

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-const counter = (function () {
-  const counterDom = document.getElementById("counter");
-  let counter = 0;
+const counter = (() => {
+  const counterDom = document.getElementById('counter');
+  let i = 0;
+  counterDom.textContent = '-';
 
   const updateDisplay = () => {
-    counterDom.textContent = counter;
+    if (i === 0) { counterDom.textContent = '-'; } else { counterDom.textContent = i; }
   };
 
   const increase = () => {
-    if (counter === 3) {
-      counter = 0;
+    if (i === 3) {
+      i = 0;
     } else {
-      counter++;
+      i++;
     }
     updateDisplay();
   };
 
   const decrease = () => {
-    if (counter === 0) {
+    if (i === 0) {
       return;
-    } else {
-      counter--;
     }
+    i--;
     updateDisplay();
   };
 
-  updateDisplay();
   return { increase, decrease };
-})();
+}());
 
-(function () {
-  const upButton = document.getElementById("up");
-  const downButton = document.getElementById("down");
+const upButton = document.getElementById('up');
+const downButton = document.getElementById('down');
 
-  upButton.addEventListener("click", counter.increase);
-  downButton.addEventListener("click", counter.decrease);
-})();
+upButton.addEventListener('click', counter.increase);
+downButton.addEventListener('click', counter.decrease);
